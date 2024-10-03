1c08d98eff3ff7ea225f0bfec5f721e91dd94ecb
unknown <syeddh01@gmail.com>
2024-10-03 15:12:18 +0530

    game completed

---
+++ src/Components/Helpers/checkWinner.js
@@ -0,0 +1,23 @@
+function isWinner(board, symbol) {
+    console.log(board, symbol)
+    // check rows
+    if(board[0] == board[1] && board[1] == board[2] && board[2] == symbol) return symbol;
+    if(board[3] == board[4] && board[4] == board[5] && board[5] == symbol) return symbol;
+    if(board[6] == board[7] && board[7] == board[8] && board[8] == symbol) return symbol;
+
+    // check columns
+    if(board[0] == board[3] && board[3] == board[6] && board[3] == symbol) return symbol;
+    if(board[1] == board[4] && board[4] == board[7] && board[4] == symbol) return symbol;
+    if(board[2] == board[5] && board[5] == board[8] && board[5] == symbol) return symbol;
+
+    // check diagonals
+    if(board[0] == board[4]  && board[4] == board[8]  && board[4] == symbol) return symbol;
+    if(board[2] == board[4]  && board[4] == board[6]  && board[4] == symbol) return symbol;
+
+    // check if board is full
+    if(board.filter((el) => el === "").length === 0) return alert("It's a tie!");
+
+    return "";
+}
+
+export default isWinner;
